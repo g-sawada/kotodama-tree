3de42f582488d77e36eb73ae53336988b8a41a11
erijawa <e.akgw0913@gmail.com>
2025-02-25 14:09:49 +0900

    [Fix] layoutからFooterのインポート文を削除
    layoutページではFooterを利用しないこととなったため、インポート文を削除しました

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { DotGothic16 } from "next/font/google"
 import "./globals.css";
 import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
 
 export const metadata: Metadata = {
   title: "コトダマプロジェクト（仮）",
