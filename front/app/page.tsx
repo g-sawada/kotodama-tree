5602e9933b68acdc93cdab083ff812b87d564b94
erijawa <e.akgw0913@gmail.com>
2025-02-26 11:05:42 +0900

    [Fix] Footerの読み込み場所をapp/mock/page.tsxに変更
    現状mockページにて試作中のため、移動しました

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,11 +1,5 @@
-import Footer from "@/components/layout/Footer";
 import React from "react";
 
 export default function Home() {
-  return (
-    <>
-      <div className="text-4xl text-center">ここはHomeページです</div>
-      <Footer />
-    </>
-  );
+  return <div className="text-4xl text-center">ここはHomeページです</div>;
 }
