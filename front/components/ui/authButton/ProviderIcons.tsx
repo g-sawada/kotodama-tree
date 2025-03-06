984939c9f86e6e9017c68b017e53e7b7754120f4
g.sawada <sawada.gota@gmail.com>
2025-03-07 01:33:36 +0900

    [fix]ProviderIcons.tsx内の各コンポーネントを関数宣言に修正

--- front/components/ui/authButton/ProviderIcons.tsx
+++ front/components/ui/authButton/ProviderIcons.tsx
@@ -1,5 +1,5 @@
 // https://tailwindflex.com/@shakti/google-login-signup-button
-export const GitHubIcon = () => {
+export function GitHubIcon() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="mr-2" viewBox="0 0 1792 1792">
     <path d="M896 128q209 0 385.5 103t279.5 279.5 103 385.5q0 251-146.5 451.5t-378.5 277.5q-27 5-40-7t-13-30q0-3 .5-76.5t.5-134.5q0-97-52-142 57-6 102.5-18t94-39 81-66.5 53-105 20.5-150.5q0-119-79-206 37-91-8-204-28-9-81 11t-92 44l-38 24q-93-26-192-26t-192 26q-16-11-42.5-27t-83.5-38.5-85-13.5q-45 113-8 204-79 87-79 206 0 85 20.5 150t52.5 105 80.5 67 94 39 102.5 18q-39 36-49 103-21 10-45 15t-57 5-65.5-21.5-55.5-62.5q-19-32-48.5-52t-49.5-24l-20-3q-21 0-29 4.5t-5 11.5 9 14 13 12l7 5q22 10 43.5 38t31.5 51l10 23q13 38 44 61.5t67 30 69.5 7 55.5-3.5l23-4q0 38 .5 88.5t.5 54.5q0 18-13 30t-40 7q-232-77-378.5-277.5t-146.5-451.5q0-209 103-385.5t279.5-279.5 385.5-103zm-477 1103q3-7-7-12-10-3-13 2-3 7 7 12 9 6 13-2zm31 34q7-5-2-16-10-9-16-3-7 5 2 16 10 10 16 3zm30 45q9-7 0-19-8-13-17-6-9 5 0 18t17 7zm42 42q8-8-4-19-12-12-20-3-9 8 4 19 12 12 20 3zm57 25q3-11-13-16-15-4-19 7t13 15q15 6 19-6zm63 5q0-13-17-11-16 0-16 11 0 13 17 11 16 0 16-11zm58-10q-2-11-18-9-16 3-14 15t18 8 14-14z"></path>
@@ -8,7 +8,7 @@ export const GitHubIcon = () => {
 }
 
 // https://www.svgrepo.com/svg/303108/google-icon-logo
-export const GoogleIcon = () => {
+export function GoogleIcon() {
   return (
     <svg viewBox="-3 0 262 262" xmlns="http://www.w3.org/2000/svg" width="23" height="23" preserveAspectRatio="xMidYMid" fill="#000000">
       <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
@@ -24,7 +24,7 @@ export const GoogleIcon = () => {
 }
 
 // https://icons8.jp/icons/set/the-x
-export const XIcon = () => {
+export function XIcon() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 50 50" fill="white">
       <path d="M 11 4 C 7.134 4 4 7.134 4 11 L 4 39 C 4 42.866 7.134 46 11 46 L 39 46 C 42.866 46 46 42.866 46 39 L 46 11 C 46 7.134 42.866 4 39 4 L 11 4 z M 13.085938 13 L 21.023438 13 L 26.660156 21.009766 L 33.5 13 L 36 13 L 27.789062 22.613281 L 37.914062 37 L 29.978516 37 L 23.4375 27.707031 L 15.5 37 L 13 37 L 22.308594 26.103516 L 13.085938 13 z M 16.914062 15 L 31.021484 35 L 34.085938 35 L 19.978516 15 L 16.914062 15 z"></path>
