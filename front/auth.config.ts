f25dfcee17c42ef27b2b2e9981dcf36c0d9ecaee
g.sawada <sawada.gota@gmail.com>
2025-03-08 00:22:53 +0900

    [add]認証プロバイダの追加と，sessionにproviderとprovider_id情報を渡すテスト

--- front/auth.config.ts
+++ front/auth.config.ts
@@ -1,13 +1,41 @@
-import type { NextAuthConfig } from "next-auth";
+import type { DefaultSession, NextAuthConfig } from "next-auth";
+import {} from "next-auth/jwt";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import Twitter from "next-auth/providers/twitter";
+
+// tokenとsessionにカスタムデータを追加するための型拡張
+declare module "next-auth" {
+  interface Session {
+    user: {
+      provider?: string;
+      provider_id?: string;
+    } & DefaultSession["user"];
+  }
+}
+
+// jwtはモジュールが異なるので別途型拡張
+declare module "next-auth/jwt" {
+  interface JWT {
+    provider?: string;
+    provider_id?: string;
+  }
+}
 
 export const authConfig: NextAuthConfig = {
   providers: [ 
     GitHub({ 
       clientId: process.env.AUTH_GITHUB_ID, 
       clientSecret: process.env.AUTH_GITHUB_SECRET
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET
+    }),
+    Twitter({
+      clientId: process.env.AUTH_X_ID,
+      clientSecret: process.env.AUTH_X_SECRET
     })
-  
   ],
   callbacks: {
     // JWTトークンに追加するデータをカスタマイズ name, email, imageはデフォルトで取得
@@ -15,8 +43,8 @@ export const authConfig: NextAuthConfig = {
     async jwt({token, account}) {
       // accoutが存在する（=認証ページからリダイレクトされた）場合
       if (!!account) {
-        console.log("account");
-        console.log(account);
+        token.provider = account.provider;
+        token.provider_id = account.providerAccountId
       }
       return token;
     },
@@ -24,6 +52,8 @@ export const authConfig: NextAuthConfig = {
     async session({session, token}) {
       session.user = {
         ...session.user,
+        provider: token.provider,
+        provider_id: token.provider_id,
         // NOTE: デフォルトのAdapterUser型にemail, imageが含まれているが，不要のため空文字で初期化
         email: "",
         image: "",
