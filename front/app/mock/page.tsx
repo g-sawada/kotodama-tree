fea5c2e0d21425b33fa2426b744b8b81825d1ae1
erijawa <e.akgw0913@gmail.com>
2025-03-07 10:44:36 +0900

    [fix] 変数の命名変更、Portal呼び出し用のコンポーネントを追加

--- front/app/mock/page.tsx
+++ front/app/mock/page.tsx
@@ -1,15 +1,7 @@
-import React from "react";
-import { EmblaOptionsType } from 'embla-carousel';
-import PortalButtonCarousel from "@/features/main/Portal/components/PortalButtonCarousel";
 import Footer from "@/components/layout/Footer";
-import Portal from "@/features/main/Portal/components/Portal";
-import { Pathway } from "@/types/pathway";
-import { getPathwaysByRoomUuid } from "@/lib/api/pathway/getPathways";
+import PortalButtonComponent from "@/features/main/Portal/components/PortalButtonComponent";
 
 export default async function MockPage() {
-  const thisRoomUuid = "room1" // テスト用に設置
-  const pathways = await getPathwaysByRoomUuid(thisRoomUuid)
-  const emblaOptions: EmblaOptionsType = { dragFree: true } //カルーセルに渡すオプション
   return (
     <>
       <div className="flex-auto">
@@ -17,11 +9,7 @@ export default async function MockPage() {
         <div className="flex w-full justify-center items-center h-60 gap-4">
           キの画像
         </div>
-        <PortalButtonCarousel options={emblaOptions}>
-          {pathways.map((pathway: Pathway) => (
-            <Portal key={pathway.id} thisRoomUuid={thisRoomUuid} pathway={pathway} />
-          ))}
-        </PortalButtonCarousel>
+        <PortalButtonComponent />
       </div>
       <Footer />
     </>
--- front/features/main/Portal/components/Portal.tsx
+++ front/features/main/Portal/components/Portal.tsx
@@ -9,25 +9,24 @@ type Props = {
   pathway: Pathway;
 };
 
-const componentMap: { [key: number]: React.FC } = {
+const PortalIconMap: { [key: number]: React.FC } = {
   1: PI_FilledTriangle,
   2: PI_FilledStar,
   3: PI_FilledPentagon,
 };
 
 export default function Portal({ thisRoomUuid, pathway }: Props) {
-  const hRef =
+  const targetRoomId =
   thisRoomUuid === pathway.room_1_id ? pathway.room_2_id : pathway.room_1_id;
-  console.log(hRef) // 対象のroom_idが取得できているか確認用
-  const figureType = pathway.figuretype;
-  const color = pathway.color;
-  const Component = componentMap[figureType];
+  console.log(targetRoomId) // 対象のroom_idが取得できているか確認用
+  const {figuretype, color} = pathway;
+  const PortalIcon = PortalIconMap[figuretype];
 
   return (
-    // /:room_uuidを実装後、href={hRef}に切り替え予定
+    // /:room_uuidを実装後、href={targetRoomId}に切り替え予定
     <Link href="/" width="64" height="64" className="inline-block embla__slide">
-      {/* componentMapから該当するアイコンコンポーネントを取得し、動的にレンダリングする */}
-      <Component color={color} />
+      {/* PortalIconMapから該当するアイコンコンポーネントを取得し、動的にレンダリングする */}
+      <PortalIcon color={color} />
     </Link>
   );
 }
