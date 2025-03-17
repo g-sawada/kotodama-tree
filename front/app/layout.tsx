d1cb166b34fa6d71b4b1f0fd9cdc8d14963444ae
g.sawada <sawada.gota@gmail.com>
2025-03-17 22:34:54 +0900

    [add]クライアントコンポーネントでsessionを使用するため，layoutファイルにSessionProviderを実装

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { DotGothic16 } from "next/font/google"
 import "./globals.css";
 import Header from "@/components/layout/Header";
+import { auth } from "@/auth";
+import { SessionProvider } from "next-auth/react";
 
 export const metadata: Metadata = {
   title: "コトダマプロジェクト（仮）",
@@ -10,23 +12,27 @@ export const metadata: Metadata = {
 
 const dotGothic16 = DotGothic16({ weight: '400', subsets: ['latin'] });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+
   return (
-    <html lang="ja">
-      <body className={dotGothic16.className}>
-        <div className="min-w-[360] sm:bg-gray-700 md:bg-blue-500 lg:bg-yellow-500 xl:bg-green-500">
-          <div className="container mx-auto full:h-screen border-2 border-white text-white bg-gray-900">
-            <div className="flex flex-col h-full"> 
-              <Header />
-              {children}
+      <html lang="ja">
+        <SessionProvider session={session}>
+          <body className={dotGothic16.className}>
+            <div className="min-w-[360] sm:bg-gray-700 md:bg-blue-500 lg:bg-yellow-500 xl:bg-green-500">
+              <div className="container mx-auto full:h-screen border-2 border-white text-white bg-gray-900">
+                <div className="flex flex-col h-full"> 
+                  <Header />
+                  {children}
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </body>
-    </html>
+          </body>
+        </SessionProvider>
+      </html>
   );
 }
