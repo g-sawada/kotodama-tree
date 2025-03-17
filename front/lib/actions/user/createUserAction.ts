29d6c2e04bb9d8176f45c4862156115bf076c5ba
g.sawada <sawada.gota@gmail.com>
2025-03-18 00:24:38 +0900

    [add]ユーザー作成用fetcherとserverActionを作成。User型をtypesディレクトリに作成
    今後の課題として，User型からproviderとprovider_idを除外する

---
+++ front/lib/actions/user/createUserAction.ts
@@ -0,0 +1,27 @@
+"use server"
+
+import { createUser } from "@/lib/api/user/createUser";
+import { redirect } from "next/navigation";
+
+export default async function createUserAction(formData: FormData) {
+  const name = formData.get("name") as string;
+  const provider = formData.get("provider") as string;
+  const provider_id = formData.get("provider_id") as string;
+
+  if(!name) {
+    console.error("ユーザー名が入力されていません");
+    redirect("/signup");
+  }
+
+  const result = await createUser(name, provider, provider_id);
+  console.log("result");
+  console.log(result);
+  
+  if (!result.isOk) {
+    console.error("ユーザーの作成時にエラーが発生しました");
+    redirect("/signup");
+  }
+
+  console.log("ユーザーの作成に成功しました");
+  redirect("/");
+} 
