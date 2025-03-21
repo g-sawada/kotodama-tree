df92a47f03ac0c5d279c09386fe56bc378230605
g.sawada <sawada.gota@gmail.com>
2025-03-22 02:14:10 +0900

    [add]API roomsコントローラーにshowアクションを実装
    idをパラメータとしてroomを検索し，存在する場合はpathwaysとtree.idをセットにして返すようにする

--- back/app/controllers/api/v1/rooms_controller.rb
+++ back/app/controllers/api/v1/rooms_controller.rb
@@ -1,6 +1,32 @@
 class Api::V1::RoomsController < ApplicationController
   # GET /api/v1/rooms/:id
   def show
-    return render json: {}, status: :ok
+    id = params[:id]
+
+    # idがない場合はエラー(400 Bad Request)を返す
+    if id.blank?
+      return render json: { error: "idがありません" }, status: :bad_request
+    end
+
+    room = Room.find_by(id: id)
+
+    # roomが存在する場合，pathwaysとtree(idのみ)のデータも返す
+    if room  
+      pathways = room.room_1_pathways + room.room_2_pathways
+
+      render json: { 
+        data: { 
+          room: room,
+          pathways: pathways,
+          tree: { id: room.tree.id }
+          # NOTE: 0321 現時点では最小限のデータとしてidのみを返すようにしておく
+          }
+        },
+        status: :ok
+    
+    # roomが存在しない場合は404(Not Found)を返す
+    else
+      render json: { error: "対象のroomが見つかりません room.id: #{id}" }, status: :not_found
+    end
   end
 end
