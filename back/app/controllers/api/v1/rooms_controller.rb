8304f7cb205b30dd280e70549fb38a2e53829ee2
g.sawada <sawada.gota@gmail.com>
2025-03-22 03:17:05 +0900

    [update]idが空の場合の専用レスポンスを廃止し，予期しないエラーの例外処理を実装して統合

--- back/app/controllers/api/v1/rooms_controller.rb
+++ back/app/controllers/api/v1/rooms_controller.rb
@@ -1,32 +1,30 @@
 class Api::V1::RoomsController < ApplicationController
   # GET /api/v1/rooms/:id
   def show
-    id = params[:id]
-
-    # idがない場合はエラー(400 Bad Request)を返す
-    if id.blank?
-      return render json: { error: "idがありません" }, status: :bad_request
-    end
-
-    room = Room.find_by(id: id)
-
-    # roomが存在する場合，pathwaysとtree(idのみ)のデータも返す
-    if room  
-      pathways = room.room_1_pathways + room.room_2_pathways
-
-      render json: { 
-        data: { 
-          room: room,
-          pathways: pathways,
-          tree: { id: room.tree.id }
-          # NOTE: 0321 現時点では最小限のデータとしてidのみを返すようにしておく
-          }
-        },
-        status: :ok
+    begin
+      id = params[:id]
+      room = Room.find_by(id: id)
+      # roomが存在する場合，pathwaysとtree(idのみ)のデータも返す
+      if room  
+        pathways = room.room_1_pathways + room.room_2_pathways
+        render json: { 
+          data: { 
+            room: room,
+            pathways: pathways,
+            tree: { id: room.tree.id }
+            # NOTE: 0321 現時点では最小限のデータとしてidのみを返すようにしておく
+            }
+          },
+          status: :ok
+      
+      # roomが存在しない場合は404(Not Found)を返す
+      else
+        render json: { error: "対象のroomが見つかりません room.id: #{id}" }, status: :not_found
+      end
     
-    # roomが存在しない場合は404(Not Found)を返す
-    else
-      render json: { error: "対象のroomが見つかりません room.id: #{id}" }, status: :not_found
+    # その他の予期しないエラー
+    rescue StandardError => e
+      render json: { error: e.message }, status: :internal_server_error
     end
   end
 end
