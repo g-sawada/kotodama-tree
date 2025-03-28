15fe318cba4690121b638dc3381f44022a6e2828
g.sawada <sawada.gota@gmail.com>
2025-03-28 14:43:28 +0900

    [add] frontのtypesにRoomとTreeを追加

---
+++ front/types/room.ts
@@ -0,0 +1,6 @@
+export interface Room {
+  id: string;
+  user_id: string;
+  created_at: string;
+  updated_at: string;
+}
---
+++ front/types/tree.ts
@@ -0,0 +1,11 @@
+export interface Tree {
+  id: number;
+  user_id: string;
+  room_id: string;
+  level: number;
+  exp: number;
+  last_charged_at: string;
+  image: string;
+  created_at: string;
+  updated_at: string;
+}
