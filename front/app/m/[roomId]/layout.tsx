fcc998f80409802d523e8aee39fb46c34eb0fa37
g.sawada <sawada.gota@gmail.com>
2025-03-31 23:33:22 +0900

    [refactor] メイン画面layout.tsxとEnterAuthコンポーネントを整理しコメントを入れる

--- front/app/m/[roomId]/layout.tsx
+++ front/app/m/[roomId]/layout.tsx
@@ -1,16 +1,22 @@
 import EnterAuth from "@/features/main/EnterAuth/EnterAuth";
 
+/**
+ * メイン画面（/m/:roomId) の共通レイアウトコンポーネント
+ * EnterAuthコンポーネントでpage.tsxのchildrenをラップし，入室認証を行う
+ * @prop { children: React.ReactNode }
+ * @prop { params: { roomId: string } }
+ */
+
 export default async function MainLayout({ children, params }
   : { children: React.ReactNode, params: { roomId: string };
 }) {
-  const { roomId: thisRoomId } = await params;
+  const { roomId } = await params;
 
   return (
-    <div>
-      メインレイアウト {thisRoomId}
-      <EnterAuth thisRoomId={thisRoomId}>
+    <>
+      <EnterAuth thisRoomId={roomId}>
         {children}
       </EnterAuth>
-    </div>
+    </>
   )
 }
