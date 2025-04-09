cc714e9613e663e4239ffaf3404529ea5fd4fa24
g.sawada <sawada.gota@gmail.com>
2025-04-09 14:06:09 +0900

    [update] ユーザー移動処理をuserMoveAction.tsに一本化
    api/user/userMove.tsは使用しない

--- front/lib/api/user/userMove.ts
+++ front/lib/api/user/userMove.ts
@@ -1,58 +1,61 @@
-/**
- * ユーザーの移動処理を行うAPIを呼び出す
- * @param userId ユーザー名
- * @param targetRoomId 移動先のルームID
- * @returns FetchResult<RoomId>
- */
+// 0409 patchFetcher.tsの作成により，userMoveAction.tsに処理を移動
+// ユーザー移動アクションはSSRで実行されないため，共通APIは使用しない
 
-import { FetchResult } from "@/types/fetchResult";
+// /**
+//  * ユーザーの移動処理を行うAPIを呼び出す
+//  * @param userId ユーザー名
+//  * @param targetRoomId 移動先のルームID
+//  * @returns FetchResult<RoomId>
+//  */
 
-interface RoomId {
-  room_id: string;
-}
-export const userMove = async (
-  userId: string,
-  roomId: string,
-): Promise<FetchResult<RoomId>> => {
-  try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/api/${process.env.NEXT_PUBLIC_API_VERSION}/users/${userId}/move`,
-      {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ room_id: roomId }),
-        cache: 'no-cache',
-      }
-    );
+// import { FetchResult } from "@/types/fetchResult";
 
-    const body = await res.json();
+// interface RoomId {
+//   room_id: string;
+// }
+// export const userMove = async (
+//   userId: string,
+//   roomId: string,
+// ): Promise<FetchResult<RoomId>> => {
+//   try {
+//     const res = await fetch(
+//       `${process.env.NEXT_PUBLIC_API_URL}/api/${process.env.NEXT_PUBLIC_API_VERSION}/users/${userId}/move`,
+//       {
+//         method: "PATCH",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ room_id: roomId }),
+//         cache: 'no-cache',
+//       }
+//     );
 
-    if (!res.ok) {
-      // エラーレスポンスが返ってきた場合
-      return {
-        status: res.status,
-        isOk: false,
-        body: { error: body.error }
-      };
+//     const body = await res.json();
 
-    } else {
-      // 正常系レスポンスが返ってきた場合
-      return {
-        status: res.status,
-        isOk: true,
-        body: { data: body.data, message: body.message }
-      };
-    }
+//     if (!res.ok) {
+//       // エラーレスポンスが返ってきた場合
+//       return {
+//         status: res.status,
+//         isOk: false,
+//         body: { error: body.error }
+//       };
 
-  } catch {
-    // ネットワークエラー等でresやjsonパースに異常が発生した場合
-    return {
-      status: 500,
-      isOk: false,
-      body: { error: "サーバー通信エラー" }
-    };
-  }
-};
+//     } else {
+//       // 正常系レスポンスが返ってきた場合
+//       return {
+//         status: res.status,
+//         isOk: true,
+//         body: { data: body.data, message: body.message }
+//       };
+//     }
+
+//   } catch {
+//     // ネットワークエラー等でresやjsonパースに異常が発生した場合
+//     return {
+//       status: 500,
+//       isOk: false,
+//       body: { error: "サーバー通信エラー" }
+//     };
+//   }
+// };
 
