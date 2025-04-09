64a1d9d669119ba8f38cac8b98aa43b2d8450df1
g.sawada <sawada.gota@gmail.com>
2025-04-09 13:47:30 +0900

    [update] ユーザー情報取得処理をapi/user/getUser.tsに一本化
    ユーザー情報取得処理は現状，SSRで実行されるため，server actionとしては実装しない

--- front/lib/api/user/getUser.ts
+++ front/lib/api/user/getUser.ts
@@ -1,51 +1,23 @@
+import { User } from "@/types/user";
+import { getFetch } from "../fetcher/getFetch";
+import { redirect } from "next/navigation";
+
 /**
  * userIdからユーザー情報を取得する
  */
 
-import { FetchResult } from "@/types/fetchResult";
-import { User } from "@/types/user";
-
-
 export const getUser = async (
   userId: string,
-): Promise<FetchResult<User>> => {
-  try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/api/${process.env.NEXT_PUBLIC_API_VERSION}/users/${userId}`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        cache: "no-cache",
-      }
-    );
+): Promise<User> => {
 
-    const body = await res.json();
+  const result = await getFetch<User>(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/${process.env.NEXT_PUBLIC_API_VERSION}/users/${userId}`,
+    )
 
-    if (!res.ok) {
-      // エラーレスポンスが返ってきた場合
-      return {
-        status: res.status,
-        isOk: false,
-        body: { error: body.error }
-      };
-
-    } else {
-      // 正常系レスポンスが返ってきた場合
-      return {
-        status: res.status,
-        isOk: true,
-        body: { data: body.data, message: body.message }
-      };
-    }
-
-  } catch {
-    // ネットワークエラー等でresやjsonパースに異常が発生した場合
-    return {
-      status: 500,
-      isOk: false,
-      body: { error: "サーバー通信エラー" }
-    };
+  // 仮実装。ユーザー情報が取得できない場合はloginページにリダイレクト 
+  if (!result.isOk) {
+    redirect("/login");
   }
+  const user = result.body.data;
+  return user;
 };
