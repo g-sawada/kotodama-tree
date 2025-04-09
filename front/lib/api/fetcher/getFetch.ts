13f8fbed6d80c583d23b14b85d66c3bcec009400
g.sawada <sawada.gota@gmail.com>
2025-04-09 12:40:36 +0900

    [add] 共通GETフェッチ実行関数としてgetFetch.tsを作成

---
+++ front/lib/api/fetcher/getFetch.ts
@@ -0,0 +1,42 @@
+import { FetchResult } from "@/types/fetchResult";
+
+export const getFetch = async <T> (
+  url: string,
+  ): Promise<FetchResult<T>> => {
+  try {
+    const res = await fetch(url, 
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        cache: "no-cache",
+      }
+    );
+    const body = await res.json();
+
+    if (!res.ok) {
+      // エラーレスポンスが返ってきた場合
+      return {
+        status: res.status,
+        isOk: false,
+        body: { error: body.error },
+      };
+    } else {
+      // 正常系レスポンスが返ってきた場合
+      return {
+        status: res.status,
+        isOk: true,
+        body: { data: body.data, message: body.message },
+      };
+    }
+
+  } catch {
+    // ネットワークエラー等でresやjsonパースに異常が発生した場合
+    return {
+      status: 500,
+      isOk: false,
+      body: { error: "サーバー通信エラー" },
+    };
+  }
+}
