a0c25f5932fdb93e03e7eda520e122e0592b02d5
g.sawada <sawada.gota@gmail.com>
2025-04-09 13:14:47 +0900

    [update] コトダマ作成処理をcreateSoulAction.tsに一本化
    クライアント側（ユーザー）の作成実行からのみ起動される処理のため，apiではなくserver actionsに処理を寄せる。api/sou/createSoulは使用しない

--- front/lib/api/soul/createSoul.ts
+++ front/lib/api/soul/createSoul.ts
@@ -1,57 +1,43 @@
-/**
- * コトダマを新規作成するAPIを呼び出す
- * @param content コトダマ本文
- * @param creator_id コトダマ作成者のuser_id
- * @returns FetchResult<Soul>
- */
+// 0409 postFetcher.tsの作成により，createSoulAction.tsに処理を移動
+// コトダマ作成アクションはSSRで実行されないため，共通APIは使用しない
 
-import { FetchResult } from "@/types/fetchResult";
-import { Soul } from "@/types/soul";
+// import { Soul } from "@/types/soul";
+// import { postFetch } from "../fetcher/postFetch";
+// import redirectToLastVisitRoomAction from "@/lib/actions/user/redirectToLastVisitRoom";
+// import { setFlash } from "../flash/setFlash";
 
-export const createSoul = async (
-  content: string,
-  creator_id: string
-): Promise<FetchResult<Soul>> => {
-  try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/api/${process.env.NEXT_PUBLIC_API_VERSION}/souls`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          soul: {
-            content: content,
-            creator_id: creator_id,
-          },
-        }),
-        cache: "no-cache",
-      }
-    );
-    const body = await res.json();
+// /**
+//  * コトダマを新規作成するAPI souls#createをコールする
+//  * @param content コトダマ本文
+//  * @param creator_id コトダマ作成者のuser_id
+//  * @returns Promise<Soul> (成功時)作成したコトダマ
+//  */
 
-    if (!res.ok) {
-      // エラーレスポンスが返ってきた場合
-      return {
-        status: res.status,
-        isOk: false,
-        body: { error: body.error },
-      };
-    } else {
-      // 正常系レスポンスが返ってきた場合
-      return {
-        status: res.status,
-        isOk: true,
-        body: { data: body.data, message: body.message },
-      };
-    }
-  } catch {
-    // ネットワークエラー等でresやjsonパースに異常が発生した場合
-    return {
-      status: 500,
-      isOk: false,
-      body: { error: "サーバー通信エラー" },
-    };
-  }
-};
+// export const createSoul = async (
+//   content: string,
+//   creator_id: string
+// ) => {
+//   // 引数からURLパラメータを生成
+//   const reqBody = {
+//     soul: {
+//       content: content,
+//       creator_id: creator_id,
+//     },
+//   }
+
+//   const result = await postFetch<Soul>(
+//     `${process.env.NEXT_PUBLIC_API_URL}/api/${process.env.NEXT_PUBLIC_API_VERSION}/souls`,
+//     reqBody
+//   );
+
+//   if(!result.isOk) {
+//     // エラー処理未実装。userのlast_visit_roomにリダイレクトする共通処理を実装して実行
+//     console.error("コトダマの作成時にエラーが発生しました");
+//     redirectToLastVisitRoomAction({ errorMessage: result.body.error });
+//   }
+
+//   await setFlash("success", "新しいコトダマを捧げました");
+  
+//   // 仮実装。捧げアニメーションのあと、メイン画面へ
+//   await redirectToLastVisitRoomAction()
+// };
