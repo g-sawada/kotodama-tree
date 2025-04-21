fb8a617d8e06879051af67310b52302304ed0e8e
g.sawada <sawada.gota@gmail.com>
2025-04-22 01:50:42 +0900

    [fix] HeartButtonToggleをクライアントコンポーネントに変更。セッションの取得をauth()ではなくuseSession()に変更

--- front/features/main/Favorite/components/HeartButtonToggle.tsx
+++ front/features/main/Favorite/components/HeartButtonToggle.tsx
@@ -1,18 +1,20 @@
-import { auth } from "@/auth";
+'use client';
+
 import EmptyHeartButton from "@/components/ui/EmptyHeartButton";
 import FilledHeartButton from "@/components/ui/FilledHeartButton";
 import createFavoriteAction from "@/lib/actions/favorite/createFavoriteAction";
 import destroyFavoriteAction from "@/lib/actions/favorite/destroyFavoriteAction";
 import { Soul } from "@/types/soul";
+import { useSession } from "next-auth/react";
 import { useState } from "react";
 
 type Props = {
   soul: Soul;
 };
-export default async function HeartButtonToggle({ soul }: Props) {
+export default function HeartButtonToggle({ soul }: Props) {
   const userIds: string[] = soul.favorites.map((fav) => fav.user_id);
-  const session = await auth();
-  const user_id = session.userId;
+  const session = useSession();
+  const user_id = session?.data?.user.userId;
   const isFavorite = userIds.some((id) => id === user_id);
 
   const [favorite, setFavorite] = useState(isFavorite);
