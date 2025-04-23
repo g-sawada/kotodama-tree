5d91208d24dc364c084595338743f35d27b00ba7
g.sawada <sawada.gota@gmail.com>
2025-04-23 23:24:30 +0900

    [update] いいね登録・解除アクションのエラー処理を中心に修正
    いいね→お気に入り　にする

--- front/lib/actions/favorite/destroyFavoriteAction.ts
+++ front/lib/actions/favorite/destroyFavoriteAction.ts
@@ -4,6 +4,7 @@ import { auth } from "@/auth";
 import { deleteFetch } from "@/lib/api/fetcher/deleteFetch";
 import { Favorite } from "@/types/favorite";
 import { redirect } from "next/navigation";
+import { setFlashAction } from "../flash/setFlashAction";
 
 /**
  * コトダマカードのいいね済みボタンから起動し，API favorites#destroyをコールする。
@@ -11,9 +12,10 @@ import { redirect } from "next/navigation";
  */
 
 export default async function destroyFavoriteAction(soul_id: number) {
-  // セッションからuser_idを取得
+  // セッションからユーザーIDを取得
   const session = await auth();
   if(!session?.user?.userId) {
+    await setFlashAction("error", "ユーザー情報の取得に失敗しました。\n 再ログインして下さい。")
     redirect("/login");
   }
   const user_id = session.user.userId;
@@ -23,12 +25,28 @@ export default async function destroyFavoriteAction(soul_id: number) {
     user_id: user_id
   }
 
-  // APIをコール
+  /**
+   * お気に入り解除 API favorites#destroy を実行
+   * @param soul_id - 対象コトダマのID
+   * @param user_id - ログインユーザーID
+   * 
+   * 正常系
+   * - favoriteデータを作成 (201 Created)
+   *   @returns data :Favorite  作成したfavoriteデータ
+   * 異常系
+   * - コトダマが存在しない (404 Not Found)   // ⭐️ 0425 TODO　APIにNot Foundを実装する
+   * - すでにお気に入り済み (409 Conflict)
+   * - その他のエラー (500 Internal Server Error)
+   */
   const result = await deleteFetch<Favorite>(
     `/souls/${soul_id}/favorites`,
     reqBody
   )
-
+  if(!result.isOk && result.status === 500) {
+    // 500エラーの場合，予期しないエラーとしてトップページにリダイレクト
+    await setFlashAction("error", "予期しないエラーが発生しました。")
+    redirect("/");
+  }
+  // 500エラー以外はクライアントで処理する
   return result;
-
 }
