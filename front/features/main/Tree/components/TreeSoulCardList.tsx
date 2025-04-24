3f92f6b21859c76bec66eb5feaf255c4fecacd7b
erijawa <e.akgw0913@gmail.com>
2025-04-24 14:40:28 +0900

    [fix] キのコトダマ一覧の詳細表示を廃止

--- front/features/main/Tree/components/TreeSoulCardList.tsx
+++ front/features/main/Tree/components/TreeSoulCardList.tsx
@@ -1,23 +1,54 @@
-// import * as React from 'react' paramsの取得に必要？
+"use client";
+
+import { useState } from "react";
+import { useParams } from "next/navigation";
 import SoulCard from "@/components/ui/SoulCard/SoulCard";
 import { Soul } from "@/types/soul";
 import { calculateSoulExp } from "@/lib/logic/calculateSoulExp";
 import HeartButtonToggle from "../../Favorite/components/HeartButtonToggle";
+import ResizeModal from "@/components/ui/ResizeModal";
+import harvestSoulAction from "@/lib/actions/soul/harvestSoulAction";
+import Button from "@/components/ui/Button";
 
 type Props = {
   souls: Soul[];
-  setSelectedSoul: (soul: Soul) => void;
   isRoomOwner: boolean;
+  canHarvest: boolean;
 };
 
-export default function TreeSoulCardList({ souls, setSelectedSoul, isRoomOwner }: Props) {
+export default function TreeSoulCardList({
+  souls,
+  isRoomOwner,
+  canHarvest,
+}: Props) {
+  const roomId = useParams().roomId as string; // URLパラメータからroomIdを取得
+  const [selectedSoul, setSelectedSoul] = useState<Soul | null>(null);
 
-  if(isRoomOwner) {
+  const openConfirmModal = (soul: Soul) => {
+    setSelectedSoul(soul);
+  };
+
+  const closeConfirmModal = () => {
+    setSelectedSoul(null);
+  };
+
+  if (isRoomOwner) {
     // ユーザーのホームの部屋の場合，経験値を算出して表示する
-    souls.forEach((soul) =>  {
+    souls.forEach((soul) => {
       soul.exp = calculateSoulExp(soul);
-    })
+    });
   }
+  const handleHarvestSubmit = async (soulId: number, roomId: string) => {
+    const result = await harvestSoulAction(soulId, roomId);
+    if (result?.isOk) {
+      // 成功時はクライアント側で再読み込みを実行
+      window.location.reload();
+    } else {
+      // 仮実装。
+      // 0410現在，harvestSoulAction内でリダイレクト処理を行っているが，クライアント側にエラーを出力するようにしたい
+      console.log("エラーです。");
+    }
+  };
 
   return (
     <>
@@ -27,7 +58,9 @@ export default function TreeSoulCardList({ souls, setSelectedSoul, isRoomOwner }
             <SoulCard
               key={soul.id}
               soul={soul}
-              handleCardClick={() => setSelectedSoul(soul)}
+              {...(!isRoomOwner && canHarvest
+                ? { handleCardClick: () => openConfirmModal(soul) }
+                : {})}
             >
               {/* ユーザーのホームのキの場合のみ，expを表示 */}
               {isRoomOwner && (
@@ -35,13 +68,44 @@ export default function TreeSoulCardList({ souls, setSelectedSoul, isRoomOwner }
                   exp: {soul.exp}
                 </p>
               )}
-              
+
               <div className="flex justify-between">
                 <p className="text-gray-700 text-md">by {soul.creator.name}</p>
                 <HeartButtonToggle soul={soul} />
               </div>
             </SoulCard>
           ))}
+          {/* 確認モーダル */}
+          <ResizeModal isOpen={!!selectedSoul}>
+            {selectedSoul && (
+              <>
+                <div className="my-4">
+                  <SoulCard soul={selectedSoul}>
+                    <p className="text-gray-700 text-md">
+                      by {selectedSoul.creator.name}
+                    </p>
+                  </SoulCard>
+                </div>
+                <p className="my-2 flex justify-center">
+                  このコトダマをしゅうかくしますか？
+                </p>
+                <div className="flex flex-justify-between gap-8 justify-center my-4">
+                  <Button
+                    text="キャンセル"
+                    handleClick={() => closeConfirmModal()}
+                    buttonType="cancel"
+                  />
+                  <Button
+                    text="OK"
+                    buttonType="ok"
+                    handleClick={() =>
+                      handleHarvestSubmit(selectedSoul.id, roomId)
+                    }
+                  />
+                </div>
+              </>
+            )}
+          </ResizeModal>
         </div>
       </div>
     </>
--- front/features/main/Tree/components/TreeSoulsModalController.tsx
+++ front/features/main/Tree/components/TreeSoulsModalController.tsx
@@ -8,14 +8,9 @@ import getSoulsAction from "@/lib/actions/soul/getSoulsAction";
 
 import Button from "@/components/ui/Button";
 import FullSizeModal from "@/components/ui/FullSizeModal";
-import SoulDetailCard from "@/components/ui/SoulCard/SoulDetailCard";
 import TreeImg from "@/components/ui/TreeImg";
 import TreeSoulCardList from "@/features/main/Tree/components/TreeSoulCardList";
-import ResizeModal from "@/components/ui/ResizeModal";
-import SoulCard from "@/components/ui/SoulCard/SoulCard";
-import harvestSoulAction from "@/lib/actions/soul/harvestSoulAction";
 import { Tree } from "@/types/tree";
-
 
 /**
  * キのコトダマ一覧用のモーダルコントローラー
@@ -32,18 +27,15 @@ type Props = {
 export default function TreeSoulsModalController({
   isRoomOwner = false,
   tree,
-  user
+  user,
 }: Props) {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
   const [souls, setSouls] = useState<Soul[]>([]);
-  const [selectedSoul, setSelectedSoul] = useState<Soul | null>(null);
   const router = useRouter();
-  const roomId = useParams().roomId as string;  // URLパラメータからroomIdを取得
+  const roomId = useParams().roomId as string; // URLパラメータからroomIdを取得
 
   // ユーザーのコトダマ収穫可否
-  const canHarvest = user.carrying_souls_count < user.max_carry_souls
-  
+  const canHarvest = user.carrying_souls_count < user.max_carry_souls;
 
   // モーダルの開閉制御
   const openModal = async () => {
@@ -55,33 +47,7 @@ export default function TreeSoulsModalController({
 
   const closeModal = () => {
     setIsModalOpen(false);
-    setSelectedSoul(null); // 選択中のコトダマをリセット
   };
-
-  const openConfirmModal = () => {
-    setIsConfirmModalOpen(true);
-  };
-
-  const closeComfirmModal = () => {
-    setIsConfirmModalOpen(false);
-  };
-
-  const backToList = () => {
-    setSelectedSoul(null); // 選択中のコトダマをリセット
-  };
-
-  const handleHarvestSubmit = async (soulId: number, roomId: string) => {
-    const result = await harvestSoulAction(soulId, roomId)
-    if(result?.isOk) {
-      // 成功時はクライアント側で再読み込みを実行
-      window.location.reload();
-    }else{
-      // 仮実装。
-      // 0410現在，harvestSoulAction内でリダイレクト処理を行っているが，クライアント側にエラーを出力するようにしたい
-      console.log("エラーです。")
-    }
-  }
-
 
   return (
     <>
@@ -94,68 +60,36 @@ export default function TreeSoulsModalController({
 
       <div>
         <FullSizeModal isOpen={isModalOpen}>
-          <h1 className="text-center text-xl font-bold">
-            {selectedSoul ? "コトダマ詳細" : "コトダマ一覧"}
-          </h1>
+          <h1 className="text-center text-xl font-bold">コトダマ一覧</h1>
+          {souls.length ? (
+            <>
+          {!isRoomOwner && canHarvest && (
+            <p className="text-center mt-2">タップしてしゅうかく</p>
+          )}
+          {!isRoomOwner && !canHarvest && (
+            <p className="text-center mt-2">
+              手持ちコトダマが上限値のためしゅうかくできません
+            </p>
+          )}
+          </>)
+          : (
+            <p className="text-center mt-2">コトダマがありません</p>
+          )}
           <div className="my-4">
-
-            {selectedSoul ? (
-              // 選択中のコトダマがあれば詳細画面を表示
-              <>
-                <SoulDetailCard soul={selectedSoul}>
-                  {isRoomOwner && (
-                    <p className="w-24 text-gray-700 bg-white rounded-xl px-2 text-center my-2 shadow-[0px_0px_5px_2px_#fff]">
-                      exp: {selectedSoul.exp}
-                    </p>
-                  )}
-                  <div className="flex justify-between">
-                    <p className="text-gray-700 text-md">by {selectedSoul.creator.name}</p>
-                  </div>
-                </SoulDetailCard>
-                {!isRoomOwner && (
-                  <div className="my-6">
-                    {!canHarvest && (
-                      <p className="flex justify-center text-sm text-gray-500">
-                        しゅうかく数上限に達しています
-                      </p>
-                    )}
-                    <div className="flex justify-center my-4 max-w-40 mx-auto">
-                      <Button
-                        text="しゅうかくする"
-                        handleClick={() => openConfirmModal()}
-                        isDisabled={!canHarvest}
-                        buttonType="ok"
-                      />
-                    </div>
-                  </div>
-                )}
-                <div className="flex justify-center my-4">
-                  <Button
-                    text="一覧にもどる"
-                    handleClick={backToList}
-                    buttonType="cancel"
-                  />
-                </div>
-              </>
-            ) : (
-            // 選択中のコトダマがなければ一覧を表示
-              <>
-                <TreeSoulCardList
-                  souls={souls}
-                  setSelectedSoul={setSelectedSoul}
-                  isRoomOwner={isRoomOwner}
+            <TreeSoulCardList
+              souls={souls}
+              isRoomOwner={isRoomOwner}
+              canHarvest={canHarvest}
+            />
+            {/* ユーザー自身の部屋の時のみ捧げページへのリンクを表示 */}
+            {isRoomOwner && (
+              <div className="flex justify-center my-4">
+                <Button
+                  text="コトダマを捧げる"
+                  handleClick={() => router.push(`/m/${roomId}/offer`)}
+                  buttonType="ok"
                 />
-                {/* ユーザー自身の部屋の時のみ捧げページへのリンクを表示 */}
-                {isRoomOwner && (
-                  <div className="flex justify-center my-4">
-                    <Button
-                      text="コトダマを捧げる"
-                      handleClick={() => router.push(`/m/${roomId}/offer`)}
-                      buttonType="ok"
-                    />
-                  </div>
-                )}
-              </>
+              </div>
             )}
           </div>
           <div className="flex justify-center my-4">
@@ -166,32 +100,6 @@ export default function TreeSoulsModalController({
             />
           </div>
         </FullSizeModal>
-        
-        {/* 確認モーダル */}
-        <ResizeModal isOpen={isConfirmModalOpen}>
-          {!!selectedSoul &&
-            <>
-              <div className="my-4">
-                  <SoulCard soul={selectedSoul}>
-                    <p className="text-gray-700 text-md">by {selectedSoul.creator.name}</p>
-                  </SoulCard>
-              </div>
-              <p className="my-2 flex justify-center">このコトダマをしゅうかくしますか？</p>
-              <div className="flex flex-justify-between gap-8 justify-center my-4">
-                <Button
-                  text="キャンセル"
-                  handleClick={() => closeComfirmModal()}
-                  buttonType="cancel"
-                  />
-                <Button
-                  text="OK"
-                  buttonType="ok"
-                  handleClick={() => handleHarvestSubmit(selectedSoul?.id, roomId)}
-                />
-              </div>
-            </>
-          }
-        </ResizeModal>
       </div>
     </>
   );
