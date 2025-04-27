a60ac89bf2a585595a4f871da83cd79e4f979bb5
g.sawada <sawada.gota@gmail.com>
2025-04-27 12:56:02 +0900

    [update] リセット処理実行API system#resetをbefore_actionの対象外に設定

--- back/app/controllers/api/v1/system_controller.rb
+++ back/app/controllers/api/v1/system_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::SystemController < ApplicationController
+  skip_before_action :check_maintenance, only: [:reset]
+
   # POST /api/v1/system/reset
   def reset
     result = WorldResetter.call
