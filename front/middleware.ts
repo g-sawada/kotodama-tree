2e46f6aa9841cc062e4e4da2b573d86e801e576b
g.sawada <sawada.gota@gmail.com>
2025-04-27 14:20:19 +0900

    [update] middlewareの適用パスを修正。認証ロジックを修正し，ログイン済ユーザーもログインページへアクセス可能に

--- front/middleware.ts
+++ front/middleware.ts
@@ -17,25 +17,14 @@ export default auth( async (req) => {
   const isLoginRoute = nextUrl.pathname ===  LOGIN_PATH;
   const isSignUpRoute = nextUrl.pathname ===  SIGN_UP_PATH;
 
-  // console.log('pathname', nextUrl.pathname);
-  // console.log('isAuthenticated', isAuthenticated);
-  // console.log('hasUuid', hasUuid);
-
   // ログインページにアクセスした場合
   if (isLoginRoute) {
-    console.log("ログインページにアクセスしました");
-    // 認証済みの場合
-    if (isAuthenticated) {
-        // かつユーザー未登録の場合は新規登録ページにリダイレクト
-        if(!hasUuid) {
-          console.log("未登録のため，新規登録ページにリダイレクトします");
-          return Response.redirect(new URL(SIGN_UP_PATH, nextUrl));
-        }
-        // ユーザー登録済みの場合はホームページにリダイレクト
-        console.log("ログイン済みのため，メイン画面にリダイレクトします");
-        return Response.redirect(new URL(REDIRECT_PATH_AFTER_LOGIN, nextUrl));
+    // 認証済み かつ ユーザー未登録 の場合
+    if (isAuthenticated && !hasUuid) {
+      return Response.redirect(new URL(SIGN_UP_PATH, nextUrl));
     }
-    // ログインしていない場合はそのまま表示
+
+    // 未ログイン または 登録済みの場合はそのまま表示
     return undefined;
   }
 
@@ -43,13 +32,11 @@ export default auth( async (req) => {
   if (isSignUpRoute) {
     // 認証前の場合はログインページにリダイレクト
     if (!isAuthenticated) {
-        console.log("ログインしていないため，ログインページにリダイレクトします");
         return Response.redirect(new URL(LOGIN_PATH, nextUrl));
     }
 
     // ユーザー登録済みの場合はホームページにリダイレクト
     if (hasUuid) {
-      console.log("登録済みのため，メイン画面にリダイレクトします");
       return Response.redirect(new URL(REDIRECT_PATH_AFTER_LOGIN, nextUrl));
     }
 
@@ -59,13 +46,11 @@ export default auth( async (req) => {
 
   // 認証の必要がある全てのページにアクセスした場合
   if (!isAuthenticated) {
-    console.log("未認証のため，ログインページにリダイレクトします");
     return Response.redirect(new URL(LOGIN_PATH, nextUrl));
   }
 
   // 認証済みかつユーザー登録が未完了の場合は新規登録ページにリダイレクト
   if (!hasUuid) {
-    console.log("未登録のため，新規登録ページにリダイレクトします");
     return Response.redirect(new URL(SIGN_UP_PATH, nextUrl));
   }
 
@@ -75,9 +60,12 @@ export default auth( async (req) => {
 
 // matcherでmiddlewareを適用するパスを指定
 export const config = {
+  // 参考: https://zenn.dev/hayato94087/articles/ec16174696a375
   matcher: [
     '/login',
     '/signup',
     '/loggedIn',
-    '/mock',],
+    '/m/:path*',
+    '/profile/:path*'
+  ],
 };
