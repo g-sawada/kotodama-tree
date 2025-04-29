c037586aed603f261736ca605ef041310ab96d81
g.sawada <sawada.gota@gmail.com>
2025-04-29 21:36:55 +0900

    [update] ヘッダーのタイトルを修正。デバッグ用のボタンを削除

--- front/components/layout/Header.tsx
+++ front/components/layout/Header.tsx
@@ -1,7 +1,6 @@
 import { auth } from "@/auth";
 
 import DropdownMenu from "./DropdownMenu";
-import AuthInfo from "./AuthInfo";
 
 export default async function Header () {
   const session = await auth();
@@ -10,10 +9,9 @@ export default async function Header () {
   return (
     <nav className="flex justify-between w-full border-b px-6">
       <div className="flex items-center justify-start h-16 gap-4">
-        <p className="text-xl font-bold">コトダマプロジェクト</p>
+        <p className="text-xl font-bold">コトダマノキ</p>
       </div>
       <div className="flex items-center justify-center h-16 gap-4">
-        <AuthInfo />
         <DropdownMenu userId={userId}/>
       </div>
     </nav>
