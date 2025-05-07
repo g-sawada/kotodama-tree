3fe294afb2b06604db2a67bd2941a4457139080b
g.sawada <sawada.gota@gmail.com>
2025-05-07 12:20:23 +0900

    [update] マイページに戻るボタンを設置

--- front/app/profile/[userId]/page.tsx
+++ front/app/profile/[userId]/page.tsx
@@ -9,6 +9,7 @@ import SoulModalController from "@/features/profile/components/SoulModalControll
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import { ProgressBar } from 'primereact/progressbar';
+import redirectToLastVisitRoomAction from "@/lib/actions/user/redirectToLastVisitRoom";
 
 /**
  * プロフィール画面
@@ -46,31 +47,41 @@ export default async function ProfilePage({ params }: Props) {
   // ページがログインユーザーのマイページであるかどうかを判定
   const isMyProfile = userId === currentUserId;
 
+  const backToMainPage = async () => {
+    'use server';
+    await redirectToLastVisitRoomAction();
+  }
+
   return (
     <>
-      <h1 className="mt-4 text-xl text-center">マイページ</h1>
-      <div className="grid place-content-center grid-rows-1 gap-1 mt-5 m-5">
-        <div className="row-span-1 border border-white rounded-lg mb-3">
-          <div className="text-center py-4 text-lg">{user.name}</div>
+      <div className="container mx-auto">
+        <div className="absolute">
+          <button className="ml-3 mt-3" onClick={backToMainPage}>⇐ もどる</button>
         </div>
+        <h1 className="mt-4 text-xl text-center">マイページ</h1>
+        <div className="grid place-content-center grid-rows-1 gap-1 mt-5 m-5">
+          <div className="row-span-1 border border-white rounded-lg mb-3">
+            <div className="text-center py-4 text-lg">{user.name}</div>
+          </div>
 
-        <div className="row-span-2 border border-white rounded-lg p-5 grid grid-cols-12 mb-3 ">
-          <Image src={`/${tree.image}`} width={130} height={130} alt="Tree Image" className="col-span-5"/>
-          <div className="grid grid-rows-3 col-span-7">
-            <p className="text-end rows-span-2">キのようす</p>
-            <p className="text-end rows-span-1">Lv:{tree.level}</p>
-            <div className="grid grid-cols-12 items-center gap-2">
-              <p className="col-span-2">Exp</p>
-              <ProgressBar className="col-span-10" value={tree.exp_progress_percent} showValue={false} style={{ width: "100%", height: "10px"}} />
+          <div className="row-span-2 border border-white rounded-lg p-5 grid grid-cols-12 mb-3 ">
+            <Image src={`/${tree.image}`} width={130} height={130} alt="Tree Image" className="col-span-5"/>
+            <div className="grid grid-rows-3 col-span-7">
+              <p className="text-end rows-span-2">キのようす</p>
+              <p className="text-end rows-span-1">Lv:{tree.level}</p>
+              <div className="grid grid-cols-12 items-center gap-2">
+                <p className="col-span-2">Exp</p>
+                <ProgressBar className="col-span-10" value={tree.exp_progress_percent} showValue={false} style={{ width: "100%", height: "10px"}} />
+              </div>
             </div>
           </div>
-        </div>
-        
-        <div className="row-span-2 border border-white rounded-lg p-2 py-5 grid grid-cols-12 mb-5">
-          <Image src="/soul.svg" width={90} height={90} alt="Soul Image" className="col-span-4"/>
-          <div className="col-span-8">
-            <div className="pb-3">コトダマ作成数 {souls.length} / {user.max_create_souls}</div>
-            <SoulModalController user={user} tree={tree} souls={souls} isMyProfile={isMyProfile}/>
+          
+          <div className="row-span-2 border border-white rounded-lg p-2 py-5 grid grid-cols-12 mb-5">
+            <Image src="/soul.svg" width={90} height={90} alt="Soul Image" className="col-span-4"/>
+            <div className="col-span-8">
+              <div className="pb-3">コトダマ作成数 {souls.length} / {user.max_create_souls}</div>
+              <SoulModalController user={user} tree={tree} souls={souls} isMyProfile={isMyProfile}/>
+            </div>
           </div>
         </div>
       </div>
