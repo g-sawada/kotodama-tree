e9008c4c036e9333096f5cdf0c8d0c067d042c26
g.sawada <sawada.gota@gmail.com>
2025-05-09 08:09:49 +0900

    [settings] production環境設定の追加。バックエンド，フロントエンド共デプロイ確認

--- back/config/environments/production.rb
+++ back/config/environments/production.rb
@@ -95,4 +95,8 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  
+
+  #Cloud Runのドメインを許可
+  config.hosts << /.*\.run\.app/
 end
